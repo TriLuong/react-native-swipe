777664f9dddae70eaa578826415df0641b9287b6
Tri Luong <triluongdl@gmail.com>
2021-01-09 15:14:15 +0700

    Add swiper.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  Image,
+} from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import Swipe from "./src/Swiper/index_1";
+import jobs from "./src/data";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  state = {
+    likedJobs: 0,
+    passedJobs: 0,
+  };
+
+  handleLikedJob = () => {
+    this.setState(({ likedJobs }) => ({
+      likedJobs: likedJobs + 1,
+    }));
+  };
+
+  handlePassedJob = () => {
+    this.setState(({ passedJobs }) => ({
+      passedJobs: passedJobs + 1,
+    }));
+  };
+
+  renderCards(job) {
+    return (
+      <Card title={job.jobtitle} titleStyle={{ fontSize: 14 }}>
+        <View style={{ height: 200 }}>
+          <Image
+            source={{ uri: "https://picsum.photos/200" }}
+            style={{ width: "100%", height: 200 }}
+          />
+        </View>
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+        <Text numberOfLines={4}>
+          {job.snippet.replace(/<b>/g, "").replace(/<\/b>/g, "")}
+        </Text>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards = () => {
+    return (
+      <Card title="No More cards">
+        <Button
+          title="Do something"
+          large
+          icon={{ name: "my-location" }}
+          backgroundColor="#03A9F4"
+          onPress={this.onReset}
+        />
+      </Card>
+    );
+  };
+
+  onReset = () => {};
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.statusStyle}>
+          <Text style={{ color: "red" }}>Passed: {this.state.passedJobs}</Text>
+          <Text style={{ color: "blue" }}>Like: {this.state.likedJobs}</Text>
+        </View>
+        <Swipe
+          onSwipeRight={this.handleLikedJob}
+          onSwipeLeft={this.handlePassedJob}
+          onReset={this.onReset}
+          keyProp="jobId"
+          data={jobs}
+          renderCard={this.renderCards}
+          renderNoMoreCards={this.renderNoMoreCards}
+          loop={true}
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  statusStyle: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  detailWrapper: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10,
   },
 });
+
+export default App;
---
+++ src/data/index.ts
@@ -0,0 +1,43 @@
+export default [
+  {
+    jobtitle: "Test 1",
+    company: "Test 1",
+    snippet:
+      "a <b>Java</b> Developer to join our team. This position will be responsible for design and development of <b>Java</b>... <b>Java</b> or C# Frameworks/Skills: <b>Java</b> EE, <b>Java</b> Swing or... ",
+    jobId: "83400e947276d20b",
+    formattedRelativeTime: "1 day ago",
+  },
+  {
+    jobtitle: "Test 2",
+    company: "Test 2",
+    snippet:
+      "Knowledge/Skills Strong <b>Java</b> development skills... abreast of emerging technology trends Core <b>Java</b> and <b>Java</b> Enterprise Edition (JavaEE) Strong relational... ",
+    jobId: "9d2c6535dc61d3b62",
+    formattedRelativeTime: "4 days ago",
+  },
+  {
+    jobtitle: "Test 3",
+    company: "Test 3",
+    snippet:
+      "Knowledge/Skills Strong <b>Java</b> development skills... abreast of emerging technology trends Core <b>Java</b> and <b>Java</b> Enterprise Edition (JavaEE) Strong relational... ",
+    jobId: "9d2c6535dc61d3b63",
+    formattedRelativeTime: "4 days ago",
+  },
+  {
+    jobtitle: "Test 4",
+    company: "Test 4",
+    snippet:
+      "Knowledge/Skills Strong <b>Java</b> development skills... abreast of emerging technology trends Core <b>Java</b> and <b>Java</b> Enterprise Edition (JavaEE) Strong relational... ",
+    jobId: "9d2c6535dc61d3b64",
+    formattedRelativeTime: "4 days ago",
+  },
+  {
+    jobtitle: "Test 5",
+    company: "Test 5",
+    snippet:
+      "Knowledge/Skills Strong <b>Java</b> development skills... abreast of emerging technology trends Core <b>Java</b> and <b>Java</b> Enterprise Edition (JavaEE) Strong relational... ",
+    jobId: "9d2c6535dc61d3b65",
+    formattedRelativeTime: "4 days ago",
+  },
+  // another jobs
+];
